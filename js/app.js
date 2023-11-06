3cb68fe89c4e73329457faaf1896c04f815fa6ee
mikathefinn <mikathefinn@gmail.com>
2023-11-06 12:39:50 +0200

    fade in/out, planets flexed

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// window.addEventListener('load', function () {
+//   const sectionToDisplay = document.querySelector('.toggle-view');
+//   const planetsToDisplay = document.querySelector('.planets');
+
+//   sectionToDisplay.style.opacity = 1;
+//   console.log('section fade in');
+//   // section has opacity set to 0 in css witha transition of 5s
+//   // after 5s it's 1, then after 5 secs function in setTimeout will change it to 0 again, with a 5s transition time set in css
+
+//   setTimeout(() => {
+//     sectionToDisplay.style.opacity = 0;
+//     console.log('opacity fade out');
+//     setTimeout(() => {
+//       sectionToDisplay.style.display = 'none';
+//       console.log('display:none');
+      
+//       console.log('planets display');
+//       planetsToDisplay.style.display = 'flex';
+//       // planetsToDisplay.style.flexDirection = 'column';
+//       // planetsToDisplay.style.justifyContent = 'center';
+//       // planetsToDisplay.style.alignItems = 'center';
+//       setTimeout(() => {
+//         console.log('planets fade-in');
+//         planetsToDisplay.style.opacity = 1;
+//       }, 1000); //opacity 1s after 
+//     }, 5100); // set display none to avoid layout issues
+//   }, 5000);
+// });
